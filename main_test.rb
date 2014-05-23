c1eb7f6a359cf358123c2029edcadcfc96f7e12a
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-05-23 15:16:30 +0200

    test for a number

--- main_test.rb
+++ main_test.rb
@@ -4,14 +4,15 @@ require 'test/unit'
 class MainTest < Test::Unit::TestCase
 
   def test_calculator
-    assert_equal Calculator.add,  0
+    assert_equal 0, Calculator.add("")
+    assert_equal 1, Calculator.add( "1")
   end
 
 end
 
 
 class Calculator
-  def self.add
+  def self.add(foo)
     0
   end
 
