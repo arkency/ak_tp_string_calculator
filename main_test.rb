7936a853b4cc8c0b4cedcd9d29627b2936b108d9
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-05-23 15:14:32 +0200

    works for empty string

--- main_test.rb
+++ main_test.rb
@@ -4,7 +4,15 @@ require 'test/unit'
 class MainTest < Test::Unit::TestCase
 
   def test_calculator
-    assert_equal Calculator.new,  0
+    assert_equal Calculator.add,  0
+  end
+
+end
+
+
+class Calculator
+  def self.add
+    0
   end
 
 end
