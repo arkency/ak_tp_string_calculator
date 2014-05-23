f0b28bdc7f9bcf25e1a6e4c1c0c830e8351217cb
tomaszpatrzek <tomasz.patrzek@op.pl>
2014-05-23 15:19:22 +0200

    spec for number in string

--- main_test.rb
+++ main_test.rb
@@ -6,6 +6,7 @@ class MainTest < Test::Unit::TestCase
   def test_calculator
     assert_equal 0, Calculator.add("")
     assert_equal 1, Calculator.add( "1")
+    assert_equal 1.3, Calculator.add( "1,3")
   end
 
 end
