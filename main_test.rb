54fa59a6cc52d51039df1919cd8ea16d52500d87
tomaszpatrzek <tomasz.patrzek@op.pl>
2014-05-30 17:28:23 +0200

    spec for new delimeter

--- main_test.rb
+++ main_test.rb
@@ -9,6 +9,7 @@ class MainTest < Test::Unit::TestCase
     assert_equal 4, Calculator.add( "1,3")
     assert_equal 5, Calculator.add( "1,3, 1")
     assert_equal 4, Calculator.add( "1\n3")
+    assert_equal 6, Calculator.add("//H\n1H5")
   end
 
 end
